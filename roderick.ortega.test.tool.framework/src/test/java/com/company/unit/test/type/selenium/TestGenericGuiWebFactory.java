722bef9b86c78a9bce85f9aa46cf4b5b17268956
roderick.ortega <ortegaroderick@hotmail.com>
2012-10-31 11:53:08 -0400

    ro_ttf checkin 1. added local resources frame 2. updated testurls with new address 3. updated test to use the frame helper additions

--- roderick.ortega.test.tool.framework/src/test/java/com/company/unit/test/type/selenium/TestGenericGuiWebFactory.java
+++ roderick.ortega.test.tool.framework/src/test/java/com/company/unit/test/type/selenium/TestGenericGuiWebFactory.java
@@ -1,31 +1,40 @@
 package com.company.unit.test.type.selenium;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.xml.sax.SAXException;
 
 import com.company.common.browser.SeleniumWebBrowser;
+import com.company.common.frame.XmlFrameBridge;
 import com.company.common.interfaces.browser.AbstractWebBrowserInterface;
 import com.company.common.interfaces.factory.AbstractGuiWebFactoryInterface;
+import com.company.common.object.helpers.XmlUrlBridge;
 import com.company.common.type.web.selenium.SeleniumGuiWebFactory;
-import com.company.common.types.Locator;
-import com.company.common.types.Url;
+import com.company.common.types.FrameParameters;
 
 public class TestGenericGuiWebFactory {
 	
 	private static AbstractGuiWebFactoryInterface guiWebFactory;
 	private static AbstractWebBrowserInterface webBrowser;
-	private static WebDriver webDriver;
+	private static XmlUrlBridge xmlUrlBridge;
+	private static XmlFrameBridge testAutomationFrame;
 	
 	@BeforeClass
-	public static void beforeClass() {
+	public static void beforeClass() throws ParserConfigurationException, SAXException, IOException {
 		guiWebFactory = new SeleniumGuiWebFactory();
 		SeleniumWebBrowser seleniumWebBrowser = new SeleniumWebBrowser();
-		webDriver = seleniumWebBrowser.getWebDriver();
 		webBrowser = seleniumWebBrowser;
 		guiWebFactory.setBrowser(webBrowser);
+		
+		xmlUrlBridge = new XmlUrlBridge(new File("./Resources/TestUrls.xml"));
+		testAutomationFrame = new XmlFrameBridge(new File("./Resources/Frames/TestAutomation.xml"));
 	}
 	
 	@Before
@@ -34,29 +43,17 @@ public class TestGenericGuiWebFactory {
 	}
 	
 	@Test
-	public void test01() {
-		System.out.println("#### new test [weblinks] ####");
-		Url testUrl = new Url("http://resources.qa.medplus.com/PortalAutomation/default.html");
-		webBrowser.navigate(testUrl);
-		guiWebFactory.createWebLink(new Locator("//A[@id='nav_textfields']")).select();
-		webBrowser.navigate(testUrl);
-		guiWebFactory.createWebLink(new Locator("//A[@id='nav_radiobuttons']")).select();
-		webBrowser.navigate(testUrl);
-		guiWebFactory.createWebLink(new Locator("//A[@id='nav_tables']")).select();
-	}
-	
-	@Test
-	public void test02() {
-		System.out.println("#### new test [buttons] ####");
-		Url testUrl = new Url("http://resources.qa.medplus.com/PortalAutomation/default.html");
-		webBrowser.navigate(testUrl);
-		guiWebFactory.createWebButton(new Locator("//DIV[@id='dialogBoxes']//BUTTON[@id='button_id01']")).select();
-		
-		webDriver.switchTo().alert().accept();
-		System.out.println(webDriver.getWindowHandles());
-		guiWebFactory.createWebButton(new Locator("//DIV[@id='dialogBoxes']//BUTTON[@id='button_id01']")).select();
-		webDriver.switchTo().alert().accept();
-		System.out.println(webDriver.getWindowHandles());
+	public void test01() throws Exception {
+		webBrowser.navigate(xmlUrlBridge.getUrl("Resources"));
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToTextFields"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToText"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToCheckBoxes"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToButtons"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToRadioButtons"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToLinks"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToListBoxes"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToTables"))).select();
+		guiWebFactory.createWebLink(testAutomationFrame.getTestObjectLocator(new FrameParameters("TestAutomationPage", "NavToNestedFormsAndControls"))).select();
 	}
 	
 	@AfterClass
